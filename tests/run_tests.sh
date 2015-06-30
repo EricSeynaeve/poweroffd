507fe859c165a2bd1e676e59ab59906e7c6c2208
Eric Seynaeve <github@nosperse.com>
2015-07-01 01:18:38 +0200

    Making test shell wrapper more location independent.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
-SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
+TEST_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+SCRIPT_DIR=$(realpath "$TEST_DIR/..")
 export PYTHONPATH=$SCRIPT_DIR
 
 func=''
@@ -8,4 +9,4 @@ if [[ -n ${1:-} ]]
 then
 	func="::$1"
 fi
-py.test --cov-report term-missing --cov .. ./basic_tests.py${func}
+py.test --cov-report term-missing --cov ${SCRIPT_DIR} ${TEST_DIR}/basic_tests.py${func}
