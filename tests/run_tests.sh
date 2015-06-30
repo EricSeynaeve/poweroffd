8e062205b5d15298bb7eeb9c05224b9a5ea7fbf9
Eric Seynaeve <github@nosperse.com>
2015-07-01 00:04:25 +0200

    Allowing to run a single test function.

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,4 +3,9 @@
 SCRIPT_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
 export PYTHONPATH=$SCRIPT_DIR
 
-py.test --cov-report term-missing --cov .. ./basic_tests.py
+func=''
+if [[ -n ${1:-} ]]
+then
+	func="::$1"
+fi
+py.test --cov-report term-missing --cov .. ./basic_tests.py${func}
